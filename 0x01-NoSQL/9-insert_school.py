88ab7f3c91d147ab047dca4d9f53ad31b7030f13
riley-kyule <phillipkyule@gmail.com>
2024-03-27 01:03:16 +0300

    Correction

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """ inserts a new document in a collection based on kwargs """
-    document_id - mongo_collection.insert(kwargs)
+    document_id = mongo_collection.insert(kwargs)
     return document_id
